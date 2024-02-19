32bb6c41d43ab94c8aa0bd3c78436ffa01abe097
MARVEBUKA <109809277+MARVEBUKA@users.noreply.github.com>
2024-02-19 22:03:41 +0100

    6th task

---
+++ declare-string-variables.js
@@ -0,0 +1,5 @@
+//instructions : Create two new string variables: myFirstName and myLastName and assign them the values of your first and last name, respectively.
+
+
+var myFirstName = "Marvelous";
+var myLastName = "Onyenaucheya";
