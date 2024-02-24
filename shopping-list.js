6eb10ae7b1314137d4ad7c680e5612c3eeac27de
MARVEBUKA <109809277+MARVEBUKA@users.noreply.github.com>
2024-02-24 11:28:48 +0100

    47th task

---
+++ shopping-list.js
@@ -0,0 +1,6 @@
+//Create a shopping list in the variable myList. The list should be a multi-dimensional array containing several sub-arrays.
+
+
+
+
+const myList = [["drinks:",3,"fanta","coke", "sprite"],["food:",3,"yam","bole","fish"],["clothes:",2,"shirt", "trouser"],["colours:",2,"black","red"],["size:",2,"XL","XXL"]];
